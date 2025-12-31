201ebd8bbdbfbcd66402473be7b047ed1fe3408b
singh2034 <singhaayush2034@gmail.com>
2025-12-31 22:35:09 +0530

    git ignore file added

--- variables/target/debug/deps/variables-297778b0d5d3041c.d
+++
@@ -1,5 +0,0 @@
-/home/luna/Documents/Study/rust-programming/variables/target/debug/deps/variables-297778b0d5d3041c.d: src/main.rs
-
-/home/luna/Documents/Study/rust-programming/variables/target/debug/deps/libvariables-297778b0d5d3041c.rmeta: src/main.rs
-
-src/main.rs:

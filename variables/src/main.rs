bec493964fb36aba4b450bfec6e42ae9a87792d5
singh2034 <singhaayush2034@gmail.com>
2026-01-01 11:11:36 +0530

    Variables updated

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -11,10 +11,14 @@ fn main() {
     let x = 5;
     let x = x + 1;
 
+    // value calculated till here
+
     {
+        // value used here
         let x = x * 2;
         println!("The value of x  in the inner scope is : {x}");
     }
 
+    // first value is used here
     println!("The value of x is: {x}");
 }

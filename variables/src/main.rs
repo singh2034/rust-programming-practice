be0174831628a971ff720795cb7f14484428dbe6
singh2034 <singhaayush2034@gmail.com>
2026-01-01 11:08:06 +0530

    Variables completed

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,20 @@
+// fn main() {
+//     let mut x = 5; //performed mutability to change the variable as per the need
+//     println!("the value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
+
+// shadowing
+
 fn main() {
-    let mut x = 5;
-    println!("the value of x is: {x}");
-    x = 6;
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x  in the inner scope is : {x}");
+    }
+
     println!("The value of x is: {x}");
 }

e2543cbe2edabeb8b6a5b3b0c2e961962fcd3d60
singh2034 <singhaayush2034@gmail.com>
2026-01-10 18:01:53 +0530

    update

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,5 +1,9 @@
 fn main () {
     // need to tell the rust compiler that, if the number is positive, if the answer is only positive then, u or it is neg or pos then i.
-    let guess:u32 = "42".parse().expect("Not a number");
-    println!("{guess}");
+    
+    // u = unsigned = only positive
+    let guess_unsigned:u32 = "42".parse().expect("Not a number");
+    // i = signed = postive or negative
+    let guess_signed:i32 = "-42".parse().expect("Not a number");
+    println!("Unsigned Number or Only Positive Number = {guess_unsigned} \nSigned Number = {guess_signed}");
 }

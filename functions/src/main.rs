ab7c099121e91703fb3037a067b5a182d71a0a37
singh2034 <singhaayush2034@gmail.com>
2026-01-17 17:55:52 +0530

    update

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("Hello, world!");
     another_function();
     parameter_function(5);
-    print_label_measusrement(5, 'h');
+    print_label_measurement(5, 'h');
 }
 
 fn another_function() {
@@ -13,6 +13,6 @@ fn parameter_function (x:i8) {
 println!("The value of x is : {x}");
 }
 
-fn print_label_measusrement (value:i8, unit_label: char) {
+fn print_label_measurement (value:i8, unit_label: char) {
 println!("The measurement is: {value}{unit_label}");
 }

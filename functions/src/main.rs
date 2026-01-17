4acc7970d043f70d4ed456911d0f41f9078cbd24
singh2034 <singhaayush2034@gmail.com>
2026-01-17 17:00:30 +0530

    git.sh made & updated

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("Hello, world!");
     another_function();
     parameter_function(5);
+    print_label_measusrement(5, 'h');
 }
 
 fn another_function() {
@@ -10,4 +11,8 @@ fn another_function() {
 
 fn parameter_function (x:i8) {
 println!("The value of x is : {x}");
+}
+
+fn print_label_measusrement (value:i8, unit_label: char) {
+println!("The measurement is: {value}{unit_label}");
 }

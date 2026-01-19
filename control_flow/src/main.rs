d07eb40e08db69efbef7b7baad1d0f5b19796040
singh2034 <singhaayush2034@gmail.com>
2026-01-19 16:24:23 +0530

    update of control flow & cargo ship challenge

---
+++ control_flow/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number:i8  = 3;
+    
+    if number < 5 {
+    println!("True!");
+    } else {
+    println!("False!!");
+    }
+}
--- practice/cargo_ship_challenge/src/main.rs
+++ practice/cargo_ship_challenge/src/main.rs
@@ -1,3 +1,28 @@
+/*
+The "Cargo Ship" Challenge 🚢
+You are writing a small program to manage a cargo ship's inventory.
+The Constant: Define a constant called MAX_WEIGHT with a value of 10,000 (use $u32$).
+
+The Array: Create an array called items_weight containing the weights of 3 items: 3500, 4200, and 1800.
+
+The Calculation: Calculate the total weight of these 3 items by adding them together and store it in a variable called current_total.
+
+The Tuple: Create a tuple called ship_status. It should hold:The current_total (as a number).
+
+A boolean that is true if current_total is less than or equal to MAX_WEIGHT, and false otherwise.
+
+The Shadowing: Use shadowing to convert the current_total (which is a number) into a String that says "Full" or just use a new string variable to print the status.
+
+The Print: Print a message like: "Ship weight: [total]. Safe to sail: [boolean]."
+*/
+
 fn main() {
-    println!("Hello, world!");
+    // Maximum Weight of Cargo
+    const MAX_WEIGHT:u32 = 10_000;
+    // Array containing
+    let items_weight:[u16; 3] = [3500, 4200, 1800];
+    // current total of all the weights
+    let current_total = 
+
+
 }

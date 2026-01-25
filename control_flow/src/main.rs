205b630d0835295c5500331f100fa7e88a291a39
singh2034 <singhaayush2034@gmail.com>
2026-01-25 22:33:40 +0530

    control_flow updated

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -8,11 +8,26 @@ fn main() {
     }
 
     another();
+    else_if();
 }
 
 fn another() {
     let number: i8 = 3;
     if number != 0 {
         println!("Number was something other than zero");
+    }
+}
+
+fn else_if() {
+    let number: i8 = 6;
+
+    if number % 4 == 0 {
+        println!("Number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("Number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("Number is divisible by 2");
+    } else {
+        println!("Number is not divisible by 4,3 & 2.");
     }
 }

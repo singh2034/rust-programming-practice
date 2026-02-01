b6fdf45abce17c38ab7e12389afe8bf56d484229
singh2034 <singhaayush2034@gmail.com>
2026-02-01 10:20:01 +0530

    initializing fibonacci

---
+++ practice/fibonacci/src/main.rs
@@ -0,0 +1,14 @@
+// Let's code a fibonacci sequence.
+
+// defince a signature function which takes a number n and returns a number
+
+fn fibonacci(n:u32) -> u32 {
+    // handle the zero case, if someone asks for 0th number, the answer is just 0.
+    if n == 0 {
+        return 0;
+    }
+
+    // initialise the variables, make 2 vars a&b make them mutable
+    let mut a = 0;
+    let mut b = 1;
+}
